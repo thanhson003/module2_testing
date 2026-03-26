79749c00def1d6270ce534990318d45828031bea
thanhson003 <nthanhson0404@gmail.com>
2026-03-26 09:04:43 +0700

    25/3

---
+++ calculator-next-day/src/NextDayCalculator.java
@@ -0,0 +1,32 @@
+public class NextDayCalculator {
+    public static String getNextDay(int day, int month, int year){
+        int lastDay = getLastDayOfMonth(month, year);
+
+        if (day < lastDay) {
+            day++;
+        } else {
+            day = 1;
+            if (month < 12) {
+                month++;
+            } else {
+                month = 1;
+                year++;
+            }
+        }
+        return day + "/" + month + "/" + year;
+    }
+
+    private static int getLastDayOfMonth(int month, int year) {
+        return switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
+            case 2 -> isLeapYear(year) ? 29 : 28;
+            default -> 0;
+        };
+    }
+
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+
+    }
+}
---
+++ calculator-next-day/src/NextDayCalculatorTest.java
@@ -0,0 +1,76 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NextDayCalculatorTest {
+
+    @Test
+    void nextDay1() {
+        int day = 1;
+        int month = 1;
+        int year = 2018;
+        String expected = "2/1/2018";
+        String actual = NextDayCalculator.getNextDay(day, month, year);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextDay2() {
+        int day = 31;
+        int month = 1;
+        int year = 2018;
+        String expected = "1/2/2018";
+        String actual = NextDayCalculator.getNextDay(day, month, year);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextDay3() {
+        int day = 30;
+        int month = 4;
+        int year = 2018;
+        String expected = "1/5/2018";
+        String actual = NextDayCalculator.getNextDay(day, month, year);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextDay4() {
+        int day = 28;
+        int month = 2;
+        int year = 2018;
+        String expected = "1/3/2018";
+        String actual = NextDayCalculator.getNextDay(day, month, year);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextDay5() {
+        int day = 29;
+        int month = 2;
+        int year = 2018;
+        String expected = "1/3/2018";
+        String actual = NextDayCalculator.getNextDay(day, month, year);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextDay6() {
+        int day = 31;
+        int month = 12;
+        int year = 2018;
+        String expected = "1/1/2019";
+        String actual = NextDayCalculator.getNextDay(day, month, year);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextDay7() {
+        int day = 3;
+        int month = 4;
+        int year = 2003;
+        String expected = "4/4/2003";
+        String actual = NextDayCalculator.getNextDay(day, month, year);
+        assertEquals(expected, actual);
+    }
+}
